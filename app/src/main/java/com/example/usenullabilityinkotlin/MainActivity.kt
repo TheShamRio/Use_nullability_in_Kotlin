f4de3abcc40e141e761aa54a09f8c4a7a78c0cb1
TheShamRio <xsham@inbox.ru>
2024-10-14 14:29:21 +0300

    Use the ?: Elvis operator

--- app/src/main/java/com/example/usenullabilityinkotlin/MainActivity.kt
+++ app/src/main/java/com/example/usenullabilityinkotlin/MainActivity.kt
@@ -16,11 +16,7 @@ import com.example.usenullabilityinkotlin.ui.theme.UseNullabilityInKotlinTheme
 fun main() {
     var favoriteActor: String? = "Sandra Oh"
 
-    val lengthOfName = if (favoriteActor != null) {
-        favoriteActor.length
-    } else {
-        0
-    }
+    val lengthOfName = favoriteActor?.length ?: 0
 
     println("The number of characters in your favorite actor's name is $lengthOfName.")
 }
